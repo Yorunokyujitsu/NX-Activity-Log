c8c77d52c1ebc1c12eb2a5c5b4e4288d6011a251
Damien Zhao <zdm65477730@126.com>
2025-04-29 13:11:28 +0800

    fix the time count loss due to DST
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- Application/source/nx/PlayData.cpp
+++ Application/source/nx/PlayData.cpp
@@ -102,6 +102,8 @@ namespace NX {
 
             u64 last_ts = 0;
             u64 last_clock = 0;
+            u64 new_ts = 0;
+            u64 new_clock = 0;
             bool in_before = false;
             bool done = false;
             for (size_t j = sessions[i].index; j < sessions[i].index + sessions[i].num; j++) {
@@ -127,25 +129,20 @@ namespace NX {
                     case Applet_InFocus:
                         last_ts = this->events[j]->steadyTimestamp;
                         last_clock = this->events[j]->clockTimestamp;
-                        in_before = false;
-                        if (this->events[j]->clockTimestamp < start_ts) {
-                            last_clock = start_ts;
-                            in_before = true;
-                        } else if (this->events[j]->clockTimestamp >= end_ts) {
+                        if (last_clock < start_ts) {
+                            last_ts = last_ts + start_ts - last_clock;
+                        } else if (last_clock >= end_ts) {
                             done = true;
                         }
                         break;
 
                     case Applet_OutFocus:
-                        if (this->events[j]->clockTimestamp >= end_ts) {
-                            stats->playtime += (end_ts - last_clock);
-                        } else if (this->events[j]->clockTimestamp >= start_ts) {
-                            if (in_before) {
-                                stats->playtime += (this->events[j]->clockTimestamp - last_clock);
-                            } else {
-                                stats->playtime += (this->events[j]->steadyTimestamp - last_ts);
-                            }
+                        new_ts = this->events[j]->steadyTimestamp;
+                        new_clock = this->events[j]->clockTimestamp;
+                        if (new_clock >= end_ts) {
+                            new_ts = new_ts + new_clock - end_ts;
                         }
+                        stats->playtime += (new_ts - last_ts);
 
                         // Move to last out focus (I don't know why the log has multiple)
                         while (j+1 < this->events.size()) {
@@ -420,7 +417,7 @@ namespace NX {
         for (size_t i = 0; i < sessions.size(); i++) {
             for (size_t j = sessions[i].index; j < sessions[i].index + sessions[i].num; j++) {
                 // Ignore repeated OutFocus events
-                if (this->events[j]->eventType == Applet_OutFocus && this->events[j-1]->eventType == Applet_OutFocus) {
+                if (this->events[j]->eventType == Applet_OutFocus && j-1 > 0 && this->events[j-1]->eventType == Applet_OutFocus) {
                     continue;
                 }
                 events.push_back(*this->events[j]);
